cd7c9274097e314b53f53104b8f04f72b6167f95
daniladomoroshchenov <99839938+daniladomoroshchenov@users.noreply.github.com>
2023-07-27 23:19:47 +0300

    Update main.cpp

--- kursovaya_ephemeris/main.cpp
+++ kursovaya_ephemeris/main.cpp
@@ -12,171 +12,143 @@
 
 using namespace std;
 
-// считывание сообщений длинной 168 байт
+// СЃС‡РёС‚С‹РІР°РЅРёРµ СЃРѕРѕР±С‰РµРЅРёР№ РґР»РёРЅРЅРѕР№ 168 Р±Р°Р№С‚
 
-void printGPSEphem3(GPSEphem3* g)
-{
-    printf("Эфермиды GPS (Required data + Optional data(big):\n");
+void printGPSEphem3(GPSEphem3* g) {
+    printf("Р­С„РµСЂРјРёРґС‹ GPS (Required data + Optional data(big):\n");
     printf("\t %u %u %u \n", g->req.sv, g->req.tow, g->req.flags);
     printf("\t %d %d %d \n", g->req.iodc, g->req.toc, g->req.ura);
-    printf("\t %u %d  %f \n", g->req.healthS, g->req.wn, g->req.tgd);
-
-    printf("\t %f  %f  %f \n", g->req.af2, g->req.af1, g->req.af0);
-    printf("\t %d %d  %lf \n", g->req.toe, g->req.iode, g->req.rootA);
-    printf("\t %lf  %lf  %lf \n", g->req.ecc, g->req.m0, g->req.omega0);
-
-    printf("\t %lf  %lf  %f \n", g->req.inc0, g->req.argPer, g->req.deln);
-    printf("\t %f  %f  %f \n", g->req.omegaDot, g->req.incDot, g->req.crc);
-    printf("\t %f  %f  %f  %f \n", g->req.crs, g->req.cuc, g->req.cic, g->req.cis);
-
+    printf("\t %u %d %e \n", g->req.healthS, g->req.wn, g->req.tgd);
+    printf("\t %e %e %e \n", g->req.af2, g->req.af1, g->req.af0);
+    printf("\t %d %d %e \n", g->req.toe, g->req.iode, g->req.rootA);
+    printf("\t %e %e %e \n", g->req.ecc, g->req.m0, g->req.omega0);
+    printf("\t %e %e %e \n", g->req.inc0, g->req.argPer, g->req.deln);
+    printf("\t %e %e %e \n", g->req.omegaDot, g->req.incDot, g->req.crc);
+    printf("\t %e %e %e %e \n", g->req.crs, g->req.cuc, g->req.cic, g->req.cis);
     printf("\t %u %d %d \n", g->opt.navType, g->opt.lTope, g->opt.lTopc);
-    printf("\t %lf  %f  %d \n", g->opt.dADot, g->opt.fDelnDot, g->opt.cURAoe);
+    printf("\t %e %e %d \n", g->opt.dADot, g->opt.fDelnDot, g->opt.cURAoe);
     printf("\t %d %d %d \n", g->opt.cURAoc, g->opt.cURAoc1, g->opt.cURAoc2);
-
-    printf("\t %f %f \n", g->opt.isc.Isc1.fIscL1CA, g->opt.isc.Isc1.fIscL2C);
-
-    printf("\t %f  %f \n", g->opt.isc.Isc1.fIscL5I5, g->opt.isc.Isc1.fIscL5Q5);
-    printf("\t %f  %f  %f \n", g->opt.isc.Isc2.fIscL1CP, g->opt.isc.Isc2.fIscL1CD, g->opt.DAf0);
-
-    printf("Контрольная сумма: \t Заданная: %u \n", g->cs);
+    printf("\t %e %e \n", g->opt.isc.Isc1.fIscL1CA, g->opt.isc.Isc1.fIscL2C);
+    printf("\t %e %e \n", g->opt.isc.Isc1.fIscL5I5, g->opt.isc.Isc1.fIscL5Q5);
+    printf("\t %e %e %e \n", g->opt.isc.Isc2.fIscL1CP, g->opt.isc.Isc2.fIscL1CD, g->opt.DAf0);
+    printf("РљРѕРЅС‚СЂРѕР»СЊРЅР°СЏ СЃСѓРјРјР°: \t Р—Р°РґР°РЅРЅР°СЏ: %u \n", g->cs);
 }
 
-// считывание сообщений длинноф 160 байт
+// СЃС‡РёС‚С‹РІР°РЅРёРµ СЃРѕРѕР±С‰РµРЅРёР№ РґР»РёРЅРЅРѕР№ 160 Р±Р°Р№С‚
 
-void printGPSEphem2(GPSEphem2* g)
-{
-    printf("Эфермиды GPS (Required data + Optional data(small)): \n");
+void printGPSEphem2(GPSEphem2* g) {
+printf("Р­С„РµСЂРјРёРґС‹ GPS (Required data + Optional data(small)): \n");
     printf("\t %u %u %u \n", g->req.sv, g->req.tow, g->req.flags);
     printf("\t %d %d %d \n", g->req.iodc, g->req.toc, g->req.ura);
-    printf("\t %u %d  %f \n", g->req.healthS, g->req.wn, g->req.tgd);
-
-    printf("\t %f  %f  %f \n", g->req.af2, g->req.af1, g->req.af0);
-    printf("\t %d %d  %lf \n", g->req.toe, g->req.iode, g->req.rootA);
-    printf("\t %lf  %lf  %lf \n", g->req.ecc, g->req.m0, g->req.omega0);
-
-    printf("\t %lf  %lf  %f \n", g->req.inc0, g->req.argPer, g->req.deln);
-    printf("\t %f  %f  %f \n", g->req.omegaDot, g->req.incDot, g->req.crc);
-    printf("\t %f  %f  %f  %f \n", g->req.crs, g->req.cuc, g->req.cic, g->req.cis);
-
+    printf("\t %u %d %e \n", g->req.healthS, g->req.wn, g->req.tgd);
+    printf("\t %e %e %e \n", g->req.af2, g->req.af1, g->req.af0);
+    printf("\t %d %d %e \n", g->req.toe, g->req.iode, g->req.rootA);
+    printf("\t %e %e %e \n", g->req.ecc, g->req.m0, g->req.omega0);
+    printf("\t %e %e %e \n", g->req.inc0, g->req.argPer, g->req.deln);
+    printf("\t %e %e %e \n", g->req.omegaDot, g->req.incDot, g->req.crc);
+    printf("\t %e %e %e %e \n", g->req.crs, g->req.cuc, g->req.cic, g->req.cis);
     printf("\t %u %d %d \n", g->opt.navType, g->opt.lTope, g->opt.lTopc);
-    printf("\t %lf  %f %d \n", g->opt.dADot, g->opt.fDelnDot, g->opt.cURAoe);
+    printf("\t %e %e %d \n", g->opt.dADot, g->opt.fDelnDot, g->opt.cURAoe);
     printf("\t %d %d %d \n", g->opt.cURAoc, g->opt.cURAoc1, g->opt.cURAoc2);
-
-    printf("\t %f  %f \n", g->opt.isc.fIscL1CA, g->opt.isc.fIscL2C);
-    printf("\t %f  %f \n", g->opt.isc.fIscL5I5, g->opt.isc.fIscL5Q5);
-
-    printf("Контрольная сумма: \t Заданная: %u \n", g->cs);
+    printf("\t %e %e \n", g->opt.isc.fIscL1CA, g->opt.isc.fIscL2C);
+    printf("\t %e %e \n", g->opt.isc.fIscL5I5, g->opt.isc.fIscL5Q5);
+    printf("РљРѕРЅС‚СЂРѕР»СЊРЅР°СЏ СЃСѓРјРјР°: \t Р—Р°РґР°РЅРЅР°СЏ: %u \n", g->cs);
 }
 
-// считывание сообщений длинной 123 байт
+// СЃС‡РёС‚С‹РІР°РЅРёРµ СЃРѕРѕР±С‰РµРЅРёР№ РґР»РёРЅРЅРѕР№ 123 Р±Р°Р№С‚
 
-void printGPSEphem1(GPSEphem1* g)
-{
-    printf("Эфемериды GPS (Only required data):\n");
+void printGPSEphem1(GPSEphem1* g) {
+    printf("Р­С„РµРјРµСЂРёРґС‹ GPS (Only required data):\n");
     printf("\t %u %u %u \n", g->req.sv, g->req.tow, g->req.flags);
     printf("\t %d %d %d \n", g->req.iodc, g->req.toc, g->req.ura);
-    printf("\t %u %d  %f \n", g->req.healthS, g->req.wn, g->req.tgd);
-
-    printf("\t %f  %f  %f \n", g->req.af2, g->req.af1, g->req.af0);
-    printf("\t %d %d  %lf \n", g->req.toe, g->req.iode, g->req.rootA);
-    printf("\t %lf  %lf  %lf \n", g->req.ecc, g->req.m0, g->req.omega0);
-
-    printf("\t %lf  %lf  %f \n", g->req.inc0, g->req.argPer, g->req.deln);
-    printf("\t %f  %f  %f \n", g->req.omegaDot, g->req.incDot, g->req.crc);
-    printf("\t %f  %f  %f  %f \n", g->req.crs, g->req.cuc, g->req.cic, g->req.cis);
-
-    printf("Контрольная сумма: \t Заданная: %u \n", g->cs);
+    printf("\t %u %d %e \n", g->req.healthS, g->req.wn, g->req.tgd);
+    printf("\t %e %e %e \n", g->req.af2, g->req.af1, g->req.af0);
+    printf("\t %d %d %e \n", g->req.toe, g->req.iode, g->req.rootA);
+    printf("\t %e %e %e \n", g->req.ecc, g->req.m0, g->req.omega0);
+    printf("\t %e %e %e \n", g->req.inc0, g->req.argPer, g->req.deln);
+    printf("\t %e %e %e \n", g->req.omegaDot, g->req.incDot, g->req.crc);
+    printf("\t %e %e %e %e \n", g->req.crs, g->req.cuc, g->req.cic, g->req.cis);
+    printf("РљРѕРЅС‚СЂРѕР»СЊРЅР°СЏ СЃСѓРјРјР°: \t Р—Р°РґР°РЅРЅР°СЏ: %u \n", g->cs);
 }
 
-int main()
-{
+int main() {
     setlocale(LC_ALL, "rus");
     FILE* file = fopen("rks.dat", "r+b");
     assert(file);
-    const char idef[] = {0x0A, 'G', 'E', '\0'}; // щаблон идентификатора
-    char buf[4] = {0}; // массив для текущих 4 байт сообщения
-    int count = 0; // номер сообщения
-    char mesSize[4] = {0}; // массив для дескриптора
-    unsigned char mes[173] = {0}; // массив хранения сообщения
+    const char idef[] = {0x0A, 'G', 'E', '\0'}; // С‰Р°Р±Р»РѕРЅ РёРґРµРЅС‚РёС„РёРєР°С‚РѕСЂР°
+    char buf[4] = {0}; // РјР°СЃСЃРёРІ РґР»СЏ С‚РµРєСѓС‰РёС… 4 Р±Р°Р№С‚ СЃРѕРѕР±С‰РµРЅРёСЏ
+    int count = 0; // РЅРѕРјРµСЂ СЃРѕРѕР±С‰РµРЅРёСЏ
+    char mesSize[4] = {0}; // РјР°СЃСЃРёРІ РґР»СЏ РґРµСЃРєСЂРёРїС‚РѕСЂР°
+    unsigned char mes[173] = {0}; // РјР°СЃСЃРёРІ С…СЂР°РЅРµРЅРёСЏ СЃРѕРѕР±С‰РµРЅРёСЏ
     mes[0] = 'G';
     mes[1] = 'E';
-
-    // подсчет сообщений с нужным идентификатором
-
+    // РїРѕРґСЃС‡РµС‚ СЃРѕРѕР±С‰РµРЅРёР№ СЃ РЅСѓР¶РЅС‹Рј РёРґРµРЅС‚РёС„РёРєР°С‚РѕСЂРѕРј
     fread(buf, 1, 2, file);
-    while(fread(buf + 2, 1, 1, file) > 0) // пока не конец файла
-    {
+    while(fread(buf + 2, 1, 1, file) > 0) { // РїРѕРєР° РЅРµ РєРѕРЅРµС† С„Р°Р№Р»Р°
         if (strcmp(buf, idef) == 0)
-            count++;
+        count++;
         for(int i = 0; i < 3; i++)
-            buf[i] = buf[i + 1];
+        buf[i] = buf[i + 1];
     }
-    printf("Всего сообщений: %d \n\n", count);
-
-    fseek(file, 0, SEEK_SET); // возвращаемся в начало файда
+    printf("Р’СЃРµРіРѕ СЃРѕРѕР±С‰РµРЅРёР№: %d \n\n", count);
+    fseek(file, 0, SEEK_SET); // РІРѕР·РІСЂР°С‰Р°РµРјСЃСЏ РІ РЅР°С‡Р°Р»Рѕ С„Р°Р№РґР°
     count = 0;
     fseek(file, 2, SEEK_SET);
     fread(buf, 1, 2, file);
-    while (fread(buf + 2, 1, 1, file) > 0)
-    {
-        if (strcmp(buf, idef) == 0) // Если найдено сообщение с подходящим идентификатором
-        {
+    while (fread(buf + 2, 1, 1, file) > 0) {
+        if (strcmp(buf, idef) == 0) { // Р•СЃР»Рё РЅР°Р№РґРµРЅРѕ СЃРѕРѕР±С‰РµРЅРёРµ СЃ РїРѕРґС…РѕРґСЏС‰РёРј РёРґРµРЅС‚РёС„РёРєР°С‚РѕСЂРѕРј
             count++;
-            fread(mesSize, 1, 3, file); // считываем дескриптор
-            long size = strtol(mesSize, NULL, 16); // определение размера сообщения
-            dataSize s = (size == 168) ? REQOPT_DATA2 : (size == 160) ? REQOPT_DATA1 : (size == 123) ? REQ_DATA : WRONG_DATA;
-            if (s == REQOPT_DATA2) // если нашлось сообщение размером 168 байт
-            {
+            fread(mesSize, 1, 3, file); // СЃС‡РёС‚С‹РІР°РµРј РґРµСЃРєСЂРёРїС‚РѕСЂ
+            long size = strtol(mesSize, NULL, 16); // РѕРїСЂРµРґРµР»РµРЅРёРµ СЂР°Р·РјРµСЂР° СЃРѕРѕР±С‰РµРЅРёСЏ
+            dataSize s = (size == 168) ? REQOPT_DATA2 : (size == 160) ?
+            REQOPT_DATA1 : (size == 123) ? REQ_DATA : WRONG_DATA;
+            if (s == REQOPT_DATA2) {// РµСЃР»Рё РЅР°С€Р»РѕСЃСЊ СЃРѕРѕР±С‰РµРЅРёРµ СЂР°Р·РјРµСЂРѕРј 168 Р±Р°Р№С‚
                 GPSEphem3 g;
                 fread((void*)&g, 1, 168, file);
                 if (!IS_OK(g))
-                    continue;
+                continue;
                 mes[2] = '0';
                 mes[3] = 'A';
                 mes[4] = '8';
                 memcpy(mes + 5, (void*)&g, 168);
                 printf("%d. ", count);
                 printGPSEphem3(&g);
-                printf("\t\t\t Пересчитанная: %u\n\n", cs(mes, 172));
+                printf("\t\t\t РџРµСЂРµСЃС‡РёС‚Р°РЅРЅР°СЏ: %u\n\n", cs(mes, 172));
             }
             else
-            if (s == REQOPT_DATA1) // если нашлось сообщение размером 160 байт
-            {
+            if (s == REQOPT_DATA1) { // РµСЃР»Рё РЅР°С€Р»РѕСЃСЊ СЃРѕРѕР±С‰РµРЅРёРµ СЂР°Р·РјРµСЂРѕРј 160 Р±Р°Р№С‚
                 GPSEphem2 g;
                 fread((void*)&g, 1, 160, file);
                 if (!IS_OK(g))
-                    continue;
+                continue;
                 mes[2] = '0';
                 mes[3] = 'A';
                 mes[4] = '0';
                 memcpy(mes + 5, (void*)&g, 160);
                 printf("%d. ", count);
                 printGPSEphem2(&g);
-                printf("\t\t\t Пересчитанная: %u\n\n", cs(mes, 164));
-            }
-            else
-            if (s == REQ_DATA) // если нашлось сообщение размером 123 байта
-            {
-                GPSEphem1 g;
-                fread((void*)&g, 1, 123, file);
-                if (!IS_OK(g))
+                printf("\t\t\t РџРµСЂРµСЃС‡РёС‚Р°РЅРЅР°СЏ: %u\n\n", cs(mes, 164));
+            } else {
+                if (s == REQ_DATA) { // РµСЃР»Рё РЅР°С€Р»РѕСЃСЊ СЃРѕРѕР±С‰РµРЅРёРµ СЂР°Р·РјРµСЂРѕРј 123 Р±Р°Р№С‚Р°
+                    GPSEphem1 g;
+                    fread((void*)&g, 1, 123, file);
+                    if (!IS_OK(g))
                     continue;
-                mes[2] = '0';
-                mes[3] = '7';
-                mes[4] = 'B';
-                memcpy(mes + 5, (void*)&g, 123);
-                printf("%d. ", count);
-                printGPSEphem1(&g);
-                printf("\t\t\t Пересчитанная: %u\n\n", cs(mes, 127));
-            }
-            else
-            {
-                printf("%d. Сообщение неверного размера\n\n", count);
-                continue;
+                    mes[2] = '0';
+                    mes[3] = '7';
+                    mes[4] = 'B';
+                    memcpy(mes + 5, (void*)&g, 123);
+                    printf("%d. ", count);
+                    printGPSEphem1(&g);
+                    printf("\t\t\t РџРµСЂРµСЃС‡РёС‚Р°РЅРЅР°СЏ: %u\n\n", cs(mes, 127));
+                } else {
+                    printf("%d. РЎРѕРѕР±С‰РµРЅРёРµ РЅРµРІРµСЂРЅРѕРіРѕ СЂР°Р·РјРµСЂР°\n\n", count);
+                    continue;
+                }
             }
         }
         for(int i = 0; i < 3; i++)
-            buf[i] = buf[i + 1];
+        buf[i] = buf[i + 1];
     }
     return 0;
 }
